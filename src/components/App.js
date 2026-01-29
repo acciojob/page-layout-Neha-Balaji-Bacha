67d54974b8a8094aa20337ac096fb640375e74f7
Neha Bacha <neha170603@gmail.com>
2026-01-29 11:50:09 +0530

    Add page layout and content to App component
    Updated the App component to include page layout and content.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,13 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+     
+  {<pageLayout>
+     <h1>Welcome to my website</h1>
+    <p>This is content of my website.</p>
+    <footer>2023 My Website. All rights rerved.</footer>
+  </pageLayout>
+  }
     </div>
   )
 }
