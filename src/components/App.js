5934d89637aef485a54360e6c31d1e3121afa635
Neha Bacha <neha170603@gmail.com>
2026-02-14 14:57:21 +0530

    Fix PageLayout component usage in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,18 @@
-
 import React from "react";
 import './../styles/App.css';
+import PageLayout from "./PageLayout";
 
 const App = () => {
   return (
     <div>
      
-  {<PageLayout>
+   <PageLayout>
      <h1>Welcome to my website</h1>
     <p>This is content of my website.</p>
     <footer>© 2023 My Website. All rights reserved.</footer>
   </PageLayout>
-  }
     </div>
-  )
-}
+  );
+};
 
 export default App
