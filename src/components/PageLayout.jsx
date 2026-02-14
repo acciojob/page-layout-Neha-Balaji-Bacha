d6477ebadd2ff60eb2f4dd07fc4676d3057dca4a
Neha Bacha <neha170603@gmail.com>
2026-02-14 15:35:04 +0530

    Add PageLayout component for consistent layout structure

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+
+const PageLayout = ({ header, footer, children }) => {
+  return (
+    <div>
+      {header}
+      {children}
+      {footer}
+    </div>
+  );
+};
+
+export default PageLayout;
 
