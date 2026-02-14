cafd3d1c3ef8a78505f4ee081e943c18ec3a9547
Neha Bacha <neha170603@gmail.com>
2026-02-14 15:10:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,1 @@
-import React from "react";
-import './../styles/App.css';
-import PageLayout from "./PageLayout";
 
-const App = () => {
-  return (
-    <div>
-      <PageLayout
-        header={<h1>Welcome to my website</h1>}
-        footer={<footer>© 2023 My Website. All rights reserved.</footer>}
-      >
-        <p>This is the content of my website.</p>
-      </PageLayout>
-    </div>
-  );
-};
-
-export default App;
