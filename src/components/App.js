b128eefd6780511e219ce0a5297158de71cdc265
Neha Bacha <neha170603@gmail.com>
2026-02-14 15:14:19 +0530

    Add PageLayout component to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
 
+import React from "react";
+import PageLayout from "./PageLayout";
+
+const App = () => {
+  return (
+    <PageLayout
+      header={<h1>Welcome to my website</h1>}
+      footer={<footer>© 2023 My Website. All rights reserved.</footer>}
+    >
+      <p>This is the content of my website.</p>
+    </PageLayout>
+  );
+};
+
+export default App;
